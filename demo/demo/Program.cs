06e399ffbd857d1b5edb6d3d0992da27d3f75160
NatswarChuan <Natswarchuan@gmail.com>
2021-08-18 19:47:16 +0700

    demo 2

--- demo/demo/Program.cs
+++ demo/demo/Program.cs
@@ -6,7 +6,10 @@ namespace demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+            Console.WriteLine(c);
         }
     }
 }
